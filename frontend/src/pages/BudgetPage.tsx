10290b4d80a5bf2d9a59ccd86444013d7d333d23
Raunak Singh <raunakmed123@gmail.com>
2026-01-15 22:41:57 +0530

    Add missing BudgetPage to fix Render build

---
+++ frontend/src/pages/BudgetPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Budget } from '../components/Budget';
+import './BudgetPage.css';
+
+const BudgetPage: React.FC = () => {
+  return <Budget />;
+};
+
+export default BudgetPage;
